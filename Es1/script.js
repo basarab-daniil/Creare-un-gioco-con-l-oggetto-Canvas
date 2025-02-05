383e265685de8c11dc373ed7ecb794a22cd5d94a
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2025-02-05 10:40:07 +0000

    punto 3

--- Es1/script.js
+++ Es1/script.js
@@ -1,5 +1,6 @@
 function startGame() {
     myGameArea.start();
+    myGameArea.draw(redSquare);
 }
 
 var myGameArea = {
@@ -9,5 +10,38 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20); //ogni 20 ms chiamo il metodo updateGameArea
+    },
+    draw: function (component) {
+        this.context.fillStyle = component.color;
+        this.context.fillRect(component.x, component.y, component.width, component.height);
     }
+
+}
+
+var redSquare = {
+    width: 20,
+    height: 20,
+    x: 10,
+    y: 120,
+    color: "red"
+}
+function moveup() {
+    redSquare.y -= 30;
+}
+
+function movedown() {
+    redSquare.y += 30;
+}
+
+function moveleft() {
+    redSquare.x -= 30;
+}
+
+function moveright() {
+    redSquare.x += 30;
+}
+
+function updateGameArea() {
+    myGameArea.draw(redSquare);
 }
